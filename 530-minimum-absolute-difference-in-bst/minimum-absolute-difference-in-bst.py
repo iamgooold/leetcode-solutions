73f73fa0f67cbda1653695d464e11116d2875d84
Au <auamores3@gmail.com>
2026-05-17 20:42:37 +0800

    Time: 4 ms (37.20%) | Memory: 20.8 MB (63.97%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.prev = None
+        self.min_diff = float('inf')
+        
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, node.val - self.prev)
+            self.prev = node.val
+            inorder(node.right)
+        
+        inorder(root)
+        return self.min_diff
