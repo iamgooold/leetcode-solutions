413300596ece5ba4de7006cfef8ede57072e31f9
𝔸𝕦 <auamores3@gmail.com>
2026-05-26 09:08:29 +0800

    Time: 55 ms (34.40%) | Memory: 19.2 MB (91.38%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,15 +1,14 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m, n = len(s1), len(s2)
-        if m + n!= len(s3): return False
-        
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-        
-        for i in range(m + 1):
-            for j in range(n + 1):
-                if i > 0 and s1[i-1] == s3[i+j-1]:
-                    dp[i][j] |= dp[i-1][j]
-                if j > 0 and s2[j-1] == s3[i+j-1]:
-                    dp[i][j] |= dp[i][j-1]
-        return dp[m][n]
+        n, m = len(s1), len(s2)
+        if n + m!= len(s3):
+            return False
+        dp = [False] * (m + 1)
+        dp[0] = True
+        for j in range(1, m + 1):
+            dp[j] = dp[j - 1] and s2[j - 1] == s3[j - 1]
+        for i in range(1, n + 1):
+            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
+            for j in range(1, m + 1):
+                dp[j] = (dp[j] and s1[i - 1] == s3[i + j - 1]) or (dp[j - 1] and s2[j - 1] == s3[i + j - 1])
+        return dp[m]
