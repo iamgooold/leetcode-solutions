5b787d4a634cf4d0d77ca823e8948dd569c826a7
𝔸𝕦 <auamores3@gmail.com>
2026-05-26 09:20:02 +0800

    Time: 0 ms (100.00%) | Memory: 20.9 MB (89.22%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        idx_map = {val: i for i, val in enumerate(inorder)}
+        self.post_idx = len(postorder) - 1
+
+        def build(in_left, in_right):
+            if in_left > in_right:
+                return None
+            root_val = postorder[self.post_idx]
+            self.post_idx -= 1
+            root = TreeNode(root_val)
+            idx = idx_map[root_val]
+            root.right = build(idx + 1, in_right)
+            root.left = build(in_left, idx - 1)
+            return root
+
+        return build(0, len(inorder) - 1)
