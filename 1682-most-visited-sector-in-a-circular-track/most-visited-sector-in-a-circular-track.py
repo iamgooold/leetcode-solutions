db3cc08e03c3d0376c01e6481961f6aad4a5c961
𝔸𝕦 <auamores3@gmail.com>
2026-06-08 18:14:38 +0800

    Time: 4 ms (26.32%) | Memory: 19.2 MB (72.51%) - LeetSync

---
+++ 1682-most-visited-sector-in-a-circular-track/most-visited-sector-in-a-circular-track.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mostVisited(self, n: int, rounds: List[int]) -> List[int]:
+        start, end = rounds[0], rounds[-1]
+        if start <= end:
+            return list(range(start, end + 1))
+        return list(range(1, end + 1)) + list(range(start, n + 1))
