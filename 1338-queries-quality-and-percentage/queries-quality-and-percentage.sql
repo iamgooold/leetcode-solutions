9e9bbd6e9b52e84e5487a11c02f2f7870eb780bc
𝐀𝐮 <auamores3@gmail.com>
2026-06-17 20:13:22 +0800

    Time: 369 ms (62.40%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,7 +1,5 @@
-SELECT 
-    query_name,
-    ROUND(AVG(rating / position), 2) AS quality,
-    ROUND(SUM(IF(rating < 3, 1, 0)) / COUNT(*) * 100, 2) AS poor_query_percentage
+SELECT query_name,
+       ROUND(AVG(rating / position), 2) AS quality,
+       ROUND(SUM(rating < 3) * 100.0 / COUNT(*), 2) AS poor_query_percentage
 FROM Queries
-WHERE query_name IS NOT NULL
 GROUP BY query_name;
