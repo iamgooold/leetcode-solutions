a00f949f1b43a60bc24553b5dcc9cce1784ced4a
𝐀𝐮 <auamores3@gmail.com>
2026-06-18 20:36:27 +0800

    Time: 275 ms (96.14%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,9 +1,3 @@
-SELECT 
-    x, 
-    y, 
-    z,
-    CASE 
-        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
-        ELSE 'No'
-    END AS triangle
+SELECT x, y, z,
+       IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') AS triangle
 FROM Triangle;
