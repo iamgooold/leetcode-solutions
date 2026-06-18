96e029ba9004302044c688b15ee2e310cfda3fd4
𝐀𝐮 <auamores3@gmail.com>
2026-06-18 20:43:29 +0800

    Time: 336 ms (32.19%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,9 @@
-WITH all_ids AS (
+SELECT id, COUNT(*) AS num
+FROM (
     SELECT requester_id AS id FROM RequestAccepted
     UNION ALL
-    SELECT accepter_id FROM RequestAccepted
-)
-SELECT id, COUNT(*) AS num
-FROM all_ids
+    SELECT accepter_id AS id FROM RequestAccepted
+) t
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1;
