529c8d2a0e9615bad8e22262834dc08db2ddd180
𝐀𝐮 <auamores3@gmail.com>
2026-06-18 20:35:59 +0800

    Time: 860 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,10 @@
 SELECT employee_id, department_id
 FROM Employee
-WHERE primary_flag = 'Y'
-   OR employee_id IN (
-       SELECT employee_id
-       FROM Employee
-       GROUP BY employee_id
-       HAVING COUNT(*) = 1
-   );
+GROUP BY employee_id
+HAVING COUNT(*) = 1
+
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y';
