1c5e4864213c371bd8a6809510efcc271a730115
𝐀𝐮 <auamores3@gmail.com>
2026-07-01 20:36:57 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.72%) - LeetSync

---
+++ 2226-rings-and-rods/rings-and-rods.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        rods = [set() for _ in range(10)]
+        for i in range(0, len(rings), 2):
+            color = rings[i]
+            rod = int(rings[i+1])
+            rods[rod].add(color)
+        return sum(1 for s in rods if len(s) == 3)
