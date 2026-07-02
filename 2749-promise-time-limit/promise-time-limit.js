c5fa78e61acf3bde45688ff3fbd94a7431a6f8d1
𝐀𝐮 <auamores3@gmail.com>
2026-07-02 21:25:15 +0800

    Time: 61 ms (7.44%) | Memory: 53.2 MB (93.39%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,10 @@
+var timeLimit = function(fn, t) {
+    return function(...args) {
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) => {
+                setTimeout(() => reject("Time Limit Exceeded"), t);
+            })
+        ]);
+    };
+};
