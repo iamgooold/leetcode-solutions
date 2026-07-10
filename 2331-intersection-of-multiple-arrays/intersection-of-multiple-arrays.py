131aaaea0e327ae56308e9182cfeeb5e1e131562
𝐀𝐮 <auamores3@gmail.com>
2026-07-10 10:15:56 +0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (10.14%) - LeetSync

---
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -0,0 +1,6 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        result = set(nums[0])
+        for arr in nums[1:]:
+            result &= set(arr)
+        return sorted(result)
